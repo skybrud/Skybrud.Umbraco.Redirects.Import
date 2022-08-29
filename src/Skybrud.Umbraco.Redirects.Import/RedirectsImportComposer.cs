eec3dffcf3870a2570994d064fc72c3bf5bc30ab
Anders Bjerner <abjerner@skybrud.dk>
2022-08-29 22:15:27 +0200

    Cleanup

--- src/Skybrud.Umbraco.Redirects.Import/RedirectsImportComposer.cs
+++ src/Skybrud.Umbraco.Redirects.Import/RedirectsImportComposer.cs
@@ -4,6 +4,8 @@ using Skybrud.Umbraco.Redirects.Import.Importers;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
 
+#pragma warning disable CS1591
+
 namespace Skybrud.Umbraco.Redirects.Import {
 
     public class RedirectsImportComposer : IComposer {
@@ -15,7 +17,7 @@ namespace Skybrud.Umbraco.Redirects.Import {
             // TODO: Should importers/exporters be registered manually as auto discovery may be expensive?
             builder.WithCollectionBuilder<ImporterCollectionBuilder>().Add(() => builder.TypeLoader.GetTypes<IImporter>());
             builder.WithCollectionBuilder<ExporterCollectionBuilder>().Add(() => builder.TypeLoader.GetTypes<IExporter>());
-            
+
             builder.ManifestFilters().Append<RedirectsImportManifestFilter>();
 
         }
