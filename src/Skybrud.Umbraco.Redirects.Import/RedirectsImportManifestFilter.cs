b74f6efd7b29851a4618e40b87b51f78b4c6dd97
Anders Bjerner <abjerner@skybrud.dk>
2024-01-06 22:18:21 +0100

    Updated package manifest filter to specify package ID

--- src/Skybrud.Umbraco.Redirects.Import/RedirectsImportManifestFilter.cs
+++ src/Skybrud.Umbraco.Redirects.Import/RedirectsImportManifestFilter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Reflection;
 using Umbraco.Cms.Core.Manifest;
 
 namespace Skybrud.Umbraco.Redirects.Import;
@@ -8,10 +9,12 @@ public class RedirectsImportManifestFilter : IManifestFilter {
 
     /// <inheritdoc />
     public void Filter(List<PackageManifest> manifests) {
-        manifests.Add(new PackageManifest {
+
+        // Initialize a new manifest filter for this package
+        PackageManifest manifest = new() {
             AllowPackageTelemetry = true,
             PackageName = RedirectsImportPackage.Name,
-            Version = RedirectsImportPackage.InformationalVersion,
+            Version = RedirectsImportPackage.InformationalVersion.Split('+')[0],
             BundleOptions = BundleOptions.Independent,
             Scripts = new[] {
                 $"/App_Plugins/{RedirectsImportPackage.Alias}/Scripts/App.js",
@@ -24,7 +27,21 @@ public class RedirectsImportManifestFilter : IManifestFilter {
             Stylesheets = new[] {
                 $"/App_Plugins/{RedirectsImportPackage.Alias}/Styles/Default.css"
             }
-        });
+        };
+
+        // The "PackageId" property isn't available prior to Umbraco 12, and since the package is build against
+        // Umbraco 10, we need to use reflection for setting the property value for Umbraco 12+. Ideally this
+        // shouldn't fail, but we might at least add a try/catch to be sure
+        try {
+            PropertyInfo? property = manifest.GetType().GetProperty("PackageId");
+            property?.SetValue(manifest, RedirectsPackage.Alias);
+        } catch {
+            // We don't really care about the exception
+        }
+
+        // Append the manifest
+        manifests.Add(manifest);
+
     }
 
 }
